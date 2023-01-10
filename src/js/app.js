cd6276610c2d99f079d997974ee8f0ab9510b78c
yamadetta <yamadetta@gmail.com>
2023-01-10 18:15:30 +0300

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,10 @@ import * as functions from "./modules/functions.js";
 functions.isWebp();
 
 
+let rightArrow = document.querySelector('#brand-carousel-right-arrow');
+let content = document.querySelector('.brand-carousel__content');
+if (rightArrow && content) {
+  rightArrow.addEventListener('click', function () {
+    content.scrollLeft = content.offsetWidth / 2;
+  });
+}
