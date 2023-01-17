7a54d2fcac1c5f0e4d690c5f898fa50033e407be
yamadetta <yamadetta@gmail.com>
2023-01-17 20:37:49 +0300

    1. Added mobile version 2. Added additional blocks

--- src/js/app.js
+++ src/js/app.js
@@ -7,12 +7,51 @@ let rightArrow = document.querySelector('#brand-carousel-right-arrow');
 let content = document.querySelector('.brand-carousel__content');
 if (rightArrow && content) {
   rightArrow.addEventListener('click', function () {
-    content.scrollLeft = content.offsetWidth / 2;
+    content.scrollLeft = content.scrollLeft + (content.offsetWidth / 2);
   });
 }
 
 document.querySelectorAll('.product__buy-button').forEach(function (item, i, arr) {
   item.addEventListener('click', function () {
     item.parentNode.classList.add('is-add-cart');
+  });
+});
+
+document.querySelectorAll('.cart-counter').forEach(function (item, i, arr) {
+  let input = item.querySelector('input');
+  let value = input.value;
+
+  item.addEventListener('click', function (e) {
+    if (e.target.textContent == '+') {
+      input.value = ++input.value;
+    } else if (e.target.textContent == '−') {
+      if (parseInt(input.value, 10) == 0) { return }
+      input.value = --input.value;
+    }
+  });
+
+  input.addEventListener('input', function (e) {
+    if (/\D/.test(e.data)) {
+      input.value = input.value.replace(/\D/ig, '');
+    }
+  });
+});
+
+document.querySelector('#menu-burger-btn').addEventListener('click', function () {
+  document.body.classList.add('menu-opened')
+});
+
+document.querySelector('#close-menu-burger-btn').addEventListener('click', function () {
+  document.body.classList.remove('menu-opened')
+});
+
+document.querySelector('#header-search-btn').addEventListener('click', function () {
+  document.body.classList.add('search-opened');
+});
+
+
+document.querySelectorAll('.js-close-mobile-search').forEach(function (item, i, arr) {
+  item.addEventListener('click', function () {
+    document.body.classList.remove('search-opened');
   });
 });
